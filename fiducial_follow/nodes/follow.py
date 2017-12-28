f2161156b9696452dcd6c28c65b093da61fd9c28
Jim Vaughan <jimv@mrjim.com>
2017-12-28 09:22:14 -0800

    Remove globals and unused imports (#7)
    Remove globals, unused imports, add some comments

--- fiducial_follow/nodes/follow.py
+++ fiducial_follow/nodes/follow.py
@@ -26,62 +26,78 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 """
-Simultaneous location and mapping based on fiducial poses.
-Receives FiducialTransform messages and builds a map and estimates the
-camera pose from them
+Fiducial Follow Demo.  Receives trasforms to fiducials and generates
+movement commands for the robot to follow the fiducial of interest.
 """
 
-
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point, Quaternion, \
-                              TransformStamped, Twist
-
-from fiducial_msgs.msg import Fiducial, FiducialTransform, FiducialTransformArray
-
-from tf.transformations import euler_from_quaternion, quaternion_slerp, \
-                               translation_matrix, quaternion_matrix, \
-                               translation_from_matrix, quaternion_from_matrix, \
-                               quaternion_from_euler
-
+from geometry_msgs.msg import TransformStamped, Twist
+from fiducial_msgs.msg import FiducialTransform, FiducialTransformArray
 import tf2_ros
-
-from math import pi, sqrt
-import sys
-import os
+from math import pi, sqrt, atan2
 import traceback
 import math
-import numpy
 import time
-import threading
-import copy
-
-#TODO: make params
-MAX_DIST=2.5
-ANGULAR_RATE=2.0
-MAX_ANGULAR_RATE=1.2
-LINEAR_RATE=1.2
-MAX_LINEAR_RATE=1.5
-HYSTERESIS_COUNT=20
-LINEAR_DECAY=0.9
-LOST_ANGULAR_RATE=0.6
-MAX_LOST_COUNT=400
 
 
 def degrees(r):
     return 180.0 * r / math.pi
 
 class Follow:
+    """
+    Constructor for our class
+    """
     def __init__(self):
        rospy.init_node('follow')
+
+       # Set up a transform listener so we can lookup transforms in the past
        self.tfBuffer = tf2_ros.Buffer(rospy.Time(30))
        self.lr = tf2_ros.TransformListener(self.tfBuffer)
        self.br = tf2_ros.TransformBroadcaster()
+
+       # A publisher for robot motion commands
        self.cmdPub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+       # The name of the coordinate frame of the fiducial we are interested in
        self.target_fiducial = rospy.get_param("~target_fiducial", "fid49")
+
+       # Minimum distance we want the robot to be from the fiducial
+       self.min_dist = rospy.get_param("~min_dist", 0.6)
+
+       # Maximum distance a fiducial can be away for us to attempt to follow
+       self.max_dist = rospy.get_param("~max_dist", 2.5)
+
+       # Proportion of angular error to use as angular velocity
+       self.angular_rate = rospy.get_param("~angular_rate", 2.0)
+
+       # Maximum angular speed (radians/second)
+       self.max_angular_rate = rospy.get_param("~max_angular_rate", 1.2)
+
+       # Angular velocity when a fiducial is not in view
+       self.lost_angular_rate = rospy.get_param("~lost_angular_rate", 0.6)
+
+       # Proportion of linear error to use as linear velocity
+       self.linear_rate = rospy.get_param("~linear_rate", 1.2)
+
+       # Maximum linear speed (meters/second)
+       self.max_linear_rate = rospy.get_param("~max_linear_rate", 1.5)
+
+       # Linear speed decay (meters/second)
+       self.linear_decay = rospy.get_param("~linear_decay", 0.9)
+
+       # How many loop iterations to keep linear velocity after fiducial
+       # disappears
+       self.hysteresis_count = rospy.get_param("~hysteresis_count", 20)
+
+       # How many loop iterations to keep rotating after fiducial disappears
+       self.max_lost_count = rospy.get_param("~max_lost_count", 400)
+
+       # Subscribe to incoming transforms
        rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.newTf)
-       self.fid_x = 0.6
+       self.fid_x = self.min_dist
        self.fid_y = 0
        self.got_fid = False
+
 
     """
     Called when a FiducialTransformArray is received
@@ -99,7 +115,7 @@ class Follow:
             trans = m.transform.translation
             rot = m.transform.rotation
             print "Fid %d %lf %lf %lf %lf %lf %lf %lf\n" % \
-                                 (id, trans.x, trans.y, trans.z, 
+                                 (id, trans.x, trans.y, trans.z,
                                   rot.x, rot.y, rot.z, rot.w)
             t = TransformStamped()
             t.child_frame_id = "fid%d" % id
@@ -114,15 +130,14 @@ class Follow:
             t.transform.rotation.w = rot.w
             self.br.sendTransform(t)
 
-            self.tfBuffer.set_transform(t, "follow")
-        
             if t.child_frame_id == self.target_fiducial:
+                self.tfBuffer.set_transform(t, "follow")
                 found = True
 
         if not found:
-            #self.got_fid = False
             return
         try:
+            # Take into account camera's pose on robot
             tf = self.tfBuffer.lookup_transform("base_link", self.target_fiducial, imageTime)
             ct = tf.transform.translation
             cr = tf.transform.rotation
@@ -132,19 +147,21 @@ class Follow:
             self.fid_y = ct.y
             self.got_fid = True
         except:
-            #traceback.print_exc()
             print "Could not get tf for %s" % self.target_fiducial
-            #self.got_fid = False
 
 
+    """
+    Main loop
+    """
     def run(self):
         rate = rospy.Rate(20)
         linSpeed = 0.0
         theta = 0.0
         times_since_last_fid = 0
 
+        # While our node is running
         while not rospy.is_shutdown():
-            forward_error = self.fid_x - 0.6
+            forward_error = self.fid_x - self.min_dist
             lateral_error = self.fid_y
             angular_error = math.atan2(self.fid_y, self.fid_x)
             print "Errors: forward %f lateral %f angular %f" % \
@@ -155,43 +172,45 @@ class Follow:
             else:
                 times_since_last_fid += 1
 
-            if forward_error > MAX_DIST:
+            if forward_error > self.max_dist:
                 print "Fiducial is too far away"
-                linSpeed = 0 
+                linSpeed = 0
                 theta = 0
             # A fiducial was detected since last iteration of this loop
             elif self.got_fid:
-                theta = angular_error * ANGULAR_RATE - theta / 2.0 
-                if theta < -MAX_ANGULAR_RATE:
-                    theta = -MAX_ANGULAR_RATE
-                if theta > MAX_ANGULAR_RATE:
-                    theta = MAX_ANGULAR_RATE
-                linSpeed = forward_error * LINEAR_RATE
-                if linSpeed < -MAX_LINEAR_RATE:
-                    linSpeed = -MAX_LINEAR_RATE
-                if linSpeed > MAX_LINEAR_RATE:
-                    linSpeed = MAX_LINEAR_RATE
+                theta = angular_error * self.angular_rate - theta / 2.0
+                if theta < -self.max_angular_rate:
+                    theta = -self.max_angular_rate
+                if theta > self.max_angular_rate:
+                    theta = self.max_angular_rate
+                linSpeed = forward_error * self.linear_rate
+                if linSpeed < -self.max_linear_rate:
+                    linSpeed = -self.max_linear_rate
+                if linSpeed > self.max_linear_rate:
+                    linSpeed = self.max_linear_rate
             # Hysteresis
-            elif not self.got_fid and times_since_last_fid < HYSTERESIS_COUNT:
-                linSpeed *= LINEAR_DECAY
+            elif not self.got_fid and times_since_last_fid < self.hysteresis_count:
+                linSpeed *= self.linear_decay
             # Try to refind fiducial by rotating
-            elif self.got_fid == False and times_since_last_fid < MAX_LOST_COUNT:
+            elif self.got_fid == False and times_since_last_fid < self.max_lost_count:
                 linSpeed = 0
                 if theta < 0:
-                    theta = -LOST_ANGULAR_RATE
+                    theta = -self.lost_angular_rate
                 elif theta > 0:
-                    theta = LOST_ANGULAR_RATE
+                    theta = self.lost_angular_rate
                 else:
                     theta = 0
                 print "Try keep rotating to refind fiducial: try# %d" % times_since_last_fid
             else:
                 theta = 0
                 linSpeed = 0
-                
+
             print "Speeds: linear %f angular %f" % (linSpeed, theta)
+
+            # Create a Twist message and publish it
             twist = Twist()
             twist.angular.z = theta
-            twist.linear.x = linSpeed 
+            twist.linear.x = linSpeed
             self.cmdPub.publish(twist)
 
             self.got_fid = False
@@ -199,5 +218,7 @@ class Follow:
 
 
 if __name__ == "__main__":
+    # Create an instance of our follow class
     node = Follow()
+    # run it
     node.run()
