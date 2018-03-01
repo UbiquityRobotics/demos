d7f65f6583e573031e830515f7ff55f12d42247f
Jim Vaughan <jimv@mrjim.com>
2018-02-28 22:08:47 -0800

    Send goals in odom frame

--- move_demo/scripts/move.py
+++ move_demo/scripts/move.py
@@ -34,9 +34,11 @@ import actionlib
 from actionlib_msgs.msg import *
 import move_base_msgs.msg
 import tf.transformations
+import tf2_ros
+import tf2_geometry_msgs
 import math
 import traceback
-from geometry_msgs.msg import Quaternion, Point
+from geometry_msgs.msg import Quaternion, Point, PoseStamped
 
 # Utility function to convert radians to degrees
 def degrees(r):
@@ -55,6 +57,10 @@ class Move:
     Constructor for our class
     """
     def __init__(self):
+        # Set up transform buffer and listener
+        self.tf_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf_buffer)
+
         # Create a proxy object for the move action server
         self.move = actionlib.SimpleActionClient('/move_base',
                                                  move_base_msgs.msg.MoveBaseAction)
@@ -68,11 +74,22 @@ class Move:
     def goto_goal(self, orientation=identity_quaternion(), position=Point()):
         if self.move is None:
             rospy.logerr("Move service not available")
+        # Transform goal into the odom fram
         # Create goal and actionlib call to rotate
+        pose_base = PoseStamped()
+        pose_base.header.frame_id = "base_link"
+        pose_base.pose.orientation = orientation
+        pose_base.pose.position = position
+
+        try:
+            pose_odom = self.tf_buffer.transform(pose_base, "odom",
+                                                 rospy.Duration(1.0))
+        except:
+            rospy.logerr("Unable to transform goal into odom frame")
+            return
         goal = move_base_msgs.msg.MoveBaseGoal()
-        goal.target_pose.header.frame_id = "base_link"
-        goal.target_pose.pose.orientation = orientation
-        goal.target_pose.pose.position = position
+        goal.target_pose.header.frame_id = "odom"
+        goal.target_pose = pose_odom
         self.move.send_goal(goal)
         self.move.wait_for_result(rospy.Duration(60.0))
 
